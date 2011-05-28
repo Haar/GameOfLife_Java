69c56e23b61d9d635cdf4a6a008e2a4b256a7b01
Robert White <haargoth@googlemail.com>
2011-05-29 00:09:47 +0100

    Environment now contains Cells, and can have specific co-ordinates set alive.

--- spec/gameOfLife/EnvironmentTest.java
+++ spec/gameOfLife/EnvironmentTest.java
@@ -1,14 +1,33 @@
 package gameOfLife;
 
-import static org.junit.Assert.assertEquals;
-import org.junit.Test;
+import static org.junit.Assert.*;
+import org.junit.*;
 
 public class EnvironmentTest {
+
+	private Environment env;
+
+	@Before
+	public void SetUpDefaulGrid()
+	{
+		env = new Environment();
+	}
 
 	@Test
 	public void testDefaultGridContainsNoLiveCells()
 	{
-		Environment env = new Environment();
-		assertEquals(env.population, 0);
+		Cell defCell = env.cellAt(0,0);
+		assertEquals(defCell.alive, false);
 	}
+
+	@Test
+	public void testGridGivenLiveCellsHasLiveCellsAtGivenCoOrdinates()
+	{
+		Coordinate[] liveCells = {new Coordinate(0, 0), new Coordinate(0,1)};
+		env = new Environment();
+		env.setLiveCells(liveCells);
+		assertEquals(env.cellAt(0,0).alive, true);
+		assertEquals(env.cellAt(0,1).alive, true);
+	}
+
 }
--- src/gameOfLife/Environment.java
+++ src/gameOfLife/Environment.java
@@ -2,6 +2,44 @@ package gameOfLife;
 
 public class Environment {
 
-	public int population;
+	private Cell[][] env;
+	private final int ROWS;
+	private final int COLS;
 
+	public Environment(int rows, int cols) {
+		env = new Cell[rows][cols];
+		ROWS = rows;
+		COLS = cols;
+		setAllCellsDead();
+	}
+
+	public Environment()
+	{
+		ROWS = 5;
+		COLS = 10;
+		env = new Cell[ROWS][COLS];
+		setAllCellsDead();
+	}
+
+	public void setLiveCells(Coordinate[] liveCells) {
+		for (Coordinate cell : liveCells)
+		{
+			env[cell.x][cell.y].alive = true;
+		}
+	}
+
+	private void setAllCellsDead()
+	{
+		for (int r = 0; r < ROWS; r++)
+		{
+			for (int c = 0; c < COLS; c++)
+			{
+				env[r][c] = new Cell();
+			}
+		}
+	}
+
+	public Cell cellAt(int i, int j) {
+		return env[i][j];
+	}
 }
