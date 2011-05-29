887599bf5554ace815c18e9963dab057332ab142
Robert White <haargoth@googlemail.com>
2011-05-30 00:12:42 +0100

    Added tests for and ability to calculate live neighbour count for two base cases.

--- spec/gameOfLife/EnvironmentTest.java
+++ spec/gameOfLife/EnvironmentTest.java
@@ -23,11 +23,25 @@ public class EnvironmentTest {
 	@Test
 	public void testGridGivenLiveCellsHasLiveCellsAtGivenCoOrdinates()
 	{
-		Coordinate[] liveCells = {new Coordinate(0, 0), new Coordinate(0,1)};
-		env = new Environment();
+		Coordinate[] liveCells = {new Coordinate(0,0), new Coordinate(0,1)};
 		env.setLiveCells(liveCells);
 		assertEquals(env.cellAt(0,0).alive, true);
 		assertEquals(env.cellAt(0,1).alive, true);
 	}
 
+	@Test
+	public void testGridCorrectlyCalculatesNeighboursForTopLeftCell()
+	{
+		Coordinate[] liveCells = {new Coordinate(0,0), new Coordinate(0,1), new Coordinate(1,0)};
+		env.setLiveCells(liveCells);
+		assertEquals(2, env.neighboursOf(0,0));
+	}
+
+	@Test
+	public void testGridCorrectlyCalculatesNeighboursForTopRightCell()
+	{
+		Coordinate[] liveCells = {new Coordinate(3,0), new Coordinate(4,0), new Coordinate (4,1)};
+		env.setLiveCells(liveCells);
+		assertEquals(2, env.neighboursOf(0, 4));
+	}
 }
--- src/gameOfLife/Environment.java
+++ src/gameOfLife/Environment.java
@@ -39,7 +39,58 @@ public class Environment {
 		}
 	}
 
-	public Cell cellAt(int i, int j) {
-		return env[i][j];
+	public Cell cellAt(int x, int y)
+	{
+		return env[x][y];
+	}
+
+	public int neighboursOf(int x, int y)
+	{
+		int neighbourCount = 0;
+
+		if (x == 0 && y == 0)
+			neighbourCount = calculateTopLeftCornerNeighbours();
+		else if (x == 0 && y == ROWS-1)
+			neighbourCount = calculateTopRightCornerNeighbours();
+
+		return neighbourCount;
+	}
+
+	private int calculateTopLeftCornerNeighbours()
+	{
+		int neighbourCount = 0;
+
+		for (int i = 0; i < 2; i++)
+		{
+			for (int j = 0; j < 2; j++)
+			{
+				if (i > 0 || j > 0)
+				{
+					if (cellAt(i,j).alive)
+						neighbourCount++;
+				}
+			}
+		}
+		return neighbourCount;
+	}
+
+	private int calculateTopRightCornerNeighbours()
+	{
+		int neighbourCount = 0;
+
+		for (int i = ROWS - 2; i < ROWS; i++)
+		{
+			for (int j = 0; j < 2; j++)
+			{
+				if  (i != (ROWS-1) || j != 0)
+				{
+					System.out.println("Row: "+i+" Col: "+j);
+					System.out.println("Cell Status: "+cellAt(i,j).alive);
+					if(cellAt(i,j).alive)
+						neighbourCount++;
+				}
+			}
+		}
+		return neighbourCount;
 	}
 }
